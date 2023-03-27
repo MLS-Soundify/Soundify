1d319298a874ad9b344ee904ea74106177d530c1
professionaljared <misterjrd22@gmail.com>
2023-03-27 13:27:50 -0400

    merge from jk-branch

--- app.js
+++ app.js
@@ -5,19 +5,48 @@ const options = {
 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
 	}
 };
-let userChoice = "beyonce"
+let userChoice = "ludracris"
 userChoice = userChoice.replace(' ', '_')
-fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${userChoice}`, options)
-	.then(response => response.json())
-	.then(response => {
-        console.log(response)
-        console.log(response.data[3].preview)
-    })
-	.catch(err => console.error(err));
 
-fetch(`https://deezerdevs-deezer.p.rapidapi.com/search/artist?q=${userChoice}`, options)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response)
-    })
-    .catch(err => console.error(err));
+
+
+const getSongName = async () => {
+    let deezerSongAPI = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${userChoice}&limit=10`, options)
+    let songNameJSON = await deezerSongAPI.json()
+    const infoArray = songNameJSON.data
+    infoArray.forEach(e => {
+        let songTitle = e.title
+        console.log(songTitle, e.preview)
+    });
+    console.log(infoArray)
+}
+getSongName()
+
+
+//broken as of now, giving back search
+const getAlbumName = async () => {
+    let deezerSongAPI = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=album:${userChoice}&limit=10`, options)
+    let songNameJSON = await deezerSongAPI.json()
+    const infoArray = songNameJSON.data
+    infoArray.forEach(e => {
+        let songTitle = e.title
+        console.log(e)
+    });
+    console.log(infoArray)
+}
+// getAlbumName()
+
+
+const playlists = [{"RandB": []}, {"Slow Jams": []},{"80's Punk": []},{"90's rap": []}] //example of how a playlist layout is
+
+/*
+TO-DO
+- make a function that adds the recently listened to songs
+    - have an array that shows the last 5 listened to songs.
+- make a function that can add liked songs
+    - have an array that stores the id of those liked songs
+- make a function that makes a new playlist
+    - make a playlist obj
+- make a function that adds songs to a playlist
+    - have a playlist storage for that obj
+*/
