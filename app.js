80dc8f1b8d29c94fdff12b0d2ff77d365d126c82
professionaljared <misterjrd22@gmail.com>
2023-03-27 09:36:08 -0400

    testing fetch calls

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '',
+		'X-RapidAPI-Key': 'bbbc3986e7msh188fe76e81fde59p1b637bjsn9ce7375c2218',
 		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
 	}
 };
-let userChoice = "Ludacris"
+let userChoice = "jay z"
+userChoice = userChoice.replace(' ', '_')
 fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${userChoice}`, options)
 	.then(response => response.json())
 	.then(response => {
-        console.log(response.data)
+        console.log(response)
+        console.log(response.data[3].preview)
     })
 	.catch(err => console.error(err));
+
+fetch(`https://deezerdevs-deezer.p.rapidapi.com/search/artist?q=${userChoice}`, options)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+    })
+    .catch(err => console.error(err));
