a9d1c72cd78d1609162276e9a8b5e457bc94fb9e
koumba-keita <koumba.keita@aol.com>
2023-03-28 15:38:13 +0000

    adding new playlist name and function for dropdown

--- tailwindConfig.js
+++ tailwindConfig.js
@@ -7,3 +7,12 @@ tailwind.config = {
       }
     }
 }
+function dropdown() {
+        document.querySelector("#submenu").classList.toggle("hidden");
+        document.querySelector("#arrow").classList.toggle("rotate-0");
+      }
+      dropdown();
+
+      function openSidebar() {
+        document.querySelector(".sidebar").classList.toggle("hidden");
+      }
